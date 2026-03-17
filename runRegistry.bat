884971247c81bd9d788918581191426ce1172d61
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-03-17 16:29:35 +0100

    runRegistry.bat: Kommentar ueber benoetige class-Files dazu

--- runRegistry.bat
+++ runRegistry.bat
@@ -1,4 +1,6 @@
 
+@REM Die RMI-Registry muss den Bytecode für die folgenden drei Klassen
+@REM kennen: ISpitznamenGenerator, SpitznamenException und SpitznamenRecord
 @set CLASSPATH=classes
 
 @rmiregistry
